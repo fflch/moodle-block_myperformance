74978855cd2532755ee2497f758cc17a09320559
Talitaventura16 <Talitaventura16@outlook.com>
2023-03-03 15:37:20 -0300

    Alterei o texto

--- block_myperformance.php
+++ block_myperformance.php
@@ -7,7 +7,7 @@ class block_myperformance extends block_base {
 	
 	public function get_content () {	
 		$this->content = new stdClass;
-		$this->content->text = "Acesse a área de desempenho dos alunos";
+		$this->content->text = "Acesse a área de desempenho dos alunos, veja aqui: ";
 		
 		return $this->content;
 		
